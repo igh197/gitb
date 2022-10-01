855186ff23f9628f654846cd82ede94225e45ad0
igh197 <igh197@daum.net>
2022-10-01 20:51:06 +0900

    project init

---
+++ personalFavoriteRestaurants/src/test/java/com/example/personalFavoriteRestaurants/PersonalFavoriteRestaurantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.personalFavoriteRestaurants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFavoriteRestaurantsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
