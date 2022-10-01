5358e5d69bf1491a45c25a4778dc89af7d278169
igh197 <57895194+igh197@users.noreply.github.com>
2022-10-01 20:57:19 +0900

    first project

---
+++ personalFavoriteRestaurants/src/main/java/com/example/personalFavoriteRestaurants/PersonalFavoriteRestaurantsApplication.java
@@ -0,0 +1,13 @@
+package com.example.personalFavoriteRestaurants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFavoriteRestaurantsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFavoriteRestaurantsApplication.class, args);
+	}
+
+}
